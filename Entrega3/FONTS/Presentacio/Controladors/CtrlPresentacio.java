b8469f1151042887295854574cac1a341cf41927
isaacroma <roma.granado@gmail.com>
2022-12-05 16:33:13 +0100

    "CtrlPresentacio"

--- Entrega3/FONTS/Presentacio/Controladors/CtrlPresentacio.java
+++ Entrega3/FONTS/Presentacio/Controladors/CtrlPresentacio.java
@@ -54,13 +54,64 @@ public class CtrlPresentacio {
         cd._ctrlDirectori.crearDirectori(id);
     }
 
-    public static void crearDocument(int id, String autor, String titol) {
-        cd.
-    }
+    public static int crearDocument(String autor, String titol, String contingut) { return cd.afegirDocument(titol, autor, contingut); }
 
     public static void obrirDocument(CtrlDirectori.FILETYPE format, String path) {
         cd.exportarDocument(format, path);
     }
+
+    public static int seleccionarDocument(int id) { return cd.seleccionarDocument(id); }
+
+    public static int afegirExp(String expressio)  { return cd.afegirExpressio(expressio); }
+
+    public static int modificarAutor(String autor) {
+        return cd.modificarAutor(autor);
+    }
+
+    public static int modificarTitol(String titol) {
+        return cd.modificarAutor(titol);
+    }
+
+    public static int modificarContingut(String contingut) {
+        return cd.modificarContingut(contingut);
+    }
+
+    public static List<Pair<String, String>> compararDocuments(CtrlDirectori.METODE_COMPARACIO m, CtrlDirectori.SORTING s, int k, int id) {
+        return cd.compararDocuments(m, s, k, id);
+    }
+
+    public static List<Pair<String, String>> compararQuery(CtrlDirectori.METODE_COMPARACIO m, CtrlDirectori.SORTING s, int k, String paraules) {
+        return cd.compararQuery(m, s, k, paraules);
+    }
+
+    public static void exportarDocument(CtrlDirectori.FILETYPE format, String path) { cd.exportarDocument(format,path); }
+
+    public static int eliminarDocument(int id){
+        return cd.eliminarDocument(id);
+    }
+
+    public static String cercaPerAutoriTitol(String autor, String titol) { return cd.cercaPerAutoriTitol(autor, titol); }
+
+    public static List<String> llistaAutorsPerPrefix(String pre , CtrlDirectori.SORTING s) { return cd.llistaAutorsPerPrefix(pre, s); }
+
+    public static List<String> llistaTitolsPerAutor(String autor, CtrlDirectori.SORTING s) { return cd.llistaTitolsPerAutor(autor,s); }
+
+    public static int seleccionarExpressio (int id) {
+        return cd.seleccionarExpressio(id);
+    }
+
+    public static int modificarExpressio(String exp){ return cd.modificarExpressio(exp); }
+
+    public static int eliminarExpressio(int id){
+        return cd.eliminarExpressio(id);
+    }
+
+    public static ArrayList<Document> selectPerExpressio(int id) { return cd.selectPerExpressio(id); }
+
+
+
+
+
 
 
 
