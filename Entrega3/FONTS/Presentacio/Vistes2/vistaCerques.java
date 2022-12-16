f0067eaed27eba23112dbe62e34bce541aa07943
isaacroma <roma.granado@gmail.com>
2022-12-16 17:36:40 +0100

    vista titols autor

---
+++ Entrega3/FONTS/Presentacio/Vistes2/vistaCerques.java
@@ -0,0 +1,42 @@
+package Presentacio.Vistes2;
+
+import Presentacio.Controladors.CtrlPresentacio;
+
+import javax.swing.*;
+
+public class vistaCerques extends JFrame {
+    private CtrlPresentacio _ctrlPresentacio;
+
+    private JPanel panel;
+    private JTextField Info;
+    private JTextArea Resultat;
+    private JComboBox Cerques;
+    private JButton Buscar;
+    private JButton Enrere;
+    private JLabel labelInfo;
+    private JLabel labelResultat;
+    private JPanel panelOpcions;
+
+    public vistaCerques() {
+        //_ctrlPresentacio = pCtrlPresentacio;
+        setContentPane(panel);
+        setBounds(450, 200, 700, 400);
+        setResizable(true);
+        setTitle("Cerca d'els titols d'un autor o dels autos que comencen per un prefix");
+
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event dispatch thread:
+        //creating and showing this application's GUI.
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                //Turn off metal's use of bold fonts
+                UIManager.put("swing.boldMetal", Boolean.FALSE);
+                new vistaCerques();
+            }
+        });
+    }
+}
