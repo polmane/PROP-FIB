92a7825d38a3703d385971a0b564e866ccc036ad
isaacroma <roma.granado@gmail.com>
2022-12-16 18:05:23 +0100

    vista documents rellevants

---
+++ Entrega3/FONTS/Presentacio/Vistes2/vistaDocumentsRellevants.java
@@ -0,0 +1,45 @@
+package Presentacio.Vistes2;
+
+import Presentacio.Controladors.CtrlPresentacio;
+
+import javax.swing.*;
+
+public class vistaDocumentsRellevants extends JFrame {
+    private CtrlPresentacio _ctrlPresentacio;
+
+    private JPanel panel;
+    private JTextField textField2;
+    private JRadioButton BOOLRadioButton;
+    private JRadioButton TFIDFRadioButton;
+    private JComboBox comboBox2;
+    private JTextArea Resultat;
+    private JButton Buscar;
+    private JButton Enrere;
+    private JPanel panelOpcions;
+    private JLabel labelResultat;
+    private JLabel labelMetode;
+    private JTextField Paraules;
+
+    public vistaDocumentsRellevants() {
+        //_ctrlPresentacio = pCtrlPresentacio;
+        setContentPane(panel);
+        setBounds(450, 200, 700, 400);
+        setResizable(true);
+        setTitle("Buscar els documents semblants");
+
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event dispatch thread:
+        //creating and showing this application's GUI.
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                //Turn off metal's use of bold fonts
+                UIManager.put("swing.boldMetal", Boolean.FALSE);
+                new vistaDocumentsRellevants();
+            }
+        });
+    }
+}
