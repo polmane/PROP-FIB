f57ecefe8ef895ee85365a21da54192fd527df23
PolCampru <pol.camprubi.prats@estudiantat.upc.edu>
2022-12-18 21:13:06 +0100

    Merge amb exportacions resolt

--- Entrega3/FONTS/Persistencia/Classes/GestorExpressions.java
+++ Entrega3/FONTS/Persistencia/Classes/GestorExpressions.java
@@ -4,42 +4,57 @@ import Domini.Classes.Pair;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
+
 
 public class GestorExpressions {
 
-    private static final String BD_PATH = "Entrega3/FONTS/Persistencia/BD";
-
-    //TODO: TEST
-    public void guardarExpressio(int idExp, String string) {
-        String nom = "Expressio" + idExp;
-        try {
-            File dir = new File(BD_PATH + "Expressions");
-            File docExp = new File(dir, nom);
-            Writer output = new BufferedWriter(new FileWriter(docExp));
-            output.write(idExp + "\n");
-            output.write(string + "\n");
-            output.flush();
-        } catch (Exception e) {
-            System.err.println("L'expressio " + idExp + ": \"" + string + "\" no s'ha guardat correctament");
-            throw new RuntimeException(e);
-        }
-    }
+    private final String BD_PATH = System.getProperty("user.dir") + "/" + "expressions";
 
     public ArrayList<Pair<Integer, String>> carregarExpressions() {
         ArrayList<Pair<Integer, String>> expressions = new ArrayList<>();
-        File dir = new File(BD_PATH + "Expressions");
+        File dir = new File(BD_PATH);
         try {
-            for (File FileExpressio : dir.listFiles()) {
+            for (File FileExpressio : Objects.requireNonNull(dir.listFiles())) {
                 Scanner scanner = new Scanner(FileExpressio);
                 int id = scanner.nextBigInteger().intValue();
                 String exp = scanner.nextLine();
                 expressions.add(new Pair<>(id, exp));
             }
         } catch (Exception e) {
-            System.err.println("No s'han pogut carregar les expressions correctament");
-            throw new RuntimeException(e);
+            e.printStackTrace();
+            Pair p = new Pair(-1,null);
+            ArrayList<Pair<Integer, String>> result = new ArrayList<>();
+            result.add(p);
+            return result;
         }
         return expressions;
+    }
+
+    public Boolean guardarExpressio (int idExp, String expressio) {
+        File experssions = new File (BD_PATH);
+        if (!experssions.exists()) {
+            experssions.mkdir();
+        }
+        try {
+            FileWriter fw = new FileWriter(BD_PATH + "/" + String.valueOf(idExp) + ".txt");
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(expressio);
+            bw.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public boolean eliminarExpressio (int idExp) {
+        File expressio = new File (BD_PATH + "/" + String.valueOf(idExp) + ".txt");
+        if (expressio.delete()) {
+            return true;
+        }
+        else return false;
     }
 }
